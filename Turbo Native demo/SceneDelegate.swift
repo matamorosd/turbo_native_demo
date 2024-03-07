342043d9420031bdabb502cd9821e1ace489e39c
d369 <dmatamoros369@gmail.com>
2024-03-07 10:13:16 -0600

    adding turbo package dependency/redirects to localhost:3000 rails app

--- Turbo Native demo/SceneDelegate.swift
+++ Turbo Native demo/SceneDelegate.swift
@@ -6,18 +6,30 @@
 //
 
 import UIKit
+import Turbo
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    private let session = Session()
+    private let navigationController = UINavigationController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        self.window?.rootViewController = UIViewController()
-        self.window?.rootViewController?.view.backgroundColor = .orange
+        self.window?.rootViewController = navigationController
+//        self.window?.rootViewController?.view.backgroundColor = .orange
         self.window?.makeKeyAndVisible()
+        
+        visit()
+    }
+    
+    private func visit() {
+        let url = URL(string: "http://localhost:3000")!
+        let controller = VisitableViewController(url: url)
+        session.visit(controller, action: .advance)
+        navigationController.pushViewController(controller, animated: true)
     }
 
 }
