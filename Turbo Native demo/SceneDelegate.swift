434c47896e050ab5fef08415536c6dfe8fb7b00b
d369 <dmatamoros369@gmail.com>
2024-03-07 10:23:20 -0600

    let turbo manage visits

--- Turbo Native demo/SceneDelegate.swift
+++ Turbo Native demo/SceneDelegate.swift
@@ -12,8 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
-    private let session = Session()
     private let navigationController = UINavigationController()
+    private lazy var session: Session = {
+        let session = Session()
+        session.delegate = self
+        return session
+    }()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -32,5 +36,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         navigationController.pushViewController(controller, animated: true)
     }
 
+}
+
+extension SceneDelegate: SessionDelegate {
+    func session(_ session: Turbo.Session, didProposeVisit proposal: Turbo.VisitProposal) {
+        let controller = VisitableViewController(url: proposal.url)
+        session.visit(controller, options: proposal.options)
+        navigationController.pushViewController(controller, animated: true)
+    }
+    
+    func session(_ session: Turbo.Session, didFailRequestForVisitable visitable: Turbo.Visitable, error: Error) {
+        // TODO: Handle errors.
+    }
+    
+    func sessionWebViewProcessDidTerminate(_ session: Turbo.Session) {
+        // TODO: Handle dead web view.
+    }
+    
+    
 }
 
